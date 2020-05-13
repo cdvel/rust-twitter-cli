6c9d5244ea7e0918865a2ffcb2bfcd9582062731
Cesar D. Velandia <cesar@velandia.co>
2020-05-13 16:11:25 +0800

    Implement sample stream @Twitter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+extern crate config;
+extern crate twitter_stream;
+
+use std::collections::HashMap;
+use twitter_stream::{Token, TwitterStreamBuilder};
+use twitter_stream::rt::{self, Future, Stream};
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut settings = config::Config::default();
+    settings.merge(config::File::with_name("Settings")).unwrap();
+
+    let secrets = settings.try_into::<HashMap<String, String>>().unwrap();
+
+    let token = Token::new( secrets.get("consumer_key").unwrap().to_owned(), 
+                            secrets.get("consumer_secret").unwrap().to_owned(),
+                            secrets.get("access_key").unwrap().to_owned(),
+                            secrets.get("access_secret").unwrap().to_owned() );
+
+    let future = TwitterStreamBuilder::filter(token)
+        .track(Some("@Twitter"))
+        .listen()
+    .unwrap()
+        .flatten_stream()
+        .for_each(|json| {
+            println!("{}", json);
+            Ok(())
+        })
+        .map_err(|e| println!("{}", e));
+
+    rt::run(future);
 }
